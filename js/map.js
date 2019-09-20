5251da8a49181188d6c36f0ff38a93978766570e
Nikhil <nikhilagarwalassisi@gmail.com>
2019-09-20 10:26:25 +0530

    basic website created and first upload

---
+++ js/map.js
@@ -0,0 +1,33 @@
+    /* ==============================================
+    MAP -->
+    =============================================== */
+    
+    var locations=[ ['<div class="infobox"><h3 class="title"><a href="#">OUR OFFICE</a></h3><span>BANGALORE</span><span></span></div>',
+    12.3341, 76.6282,
+    2]];
+    var map=new google.maps.Map(document.getElementById('map'), {
+        zoom: 14, scrollwheel: false, navigationControl: true, mapTypeControl: false, scaleControl: false, draggable: true, styles: [ {
+            "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"}]}]
+        }
+        ], center: new google.maps.LatLng(12.3341, 76.6282), mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    
+    );
+    var infowindow=new google.maps.InfoWindow();
+    var marker,
+    i;
+    for (i=0;
+    i < locations.length;
+    i++) {
+        marker=new google.maps.Marker( {
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]), map: map, icon: 'images/gps.png'
+        }
+        );
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        }
+        )(marker, i));
+    }
